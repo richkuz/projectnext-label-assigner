7fe131b11a0ef3a1a085514826d4022848ccfd4b
Rich Kuzsma <62522248+richkuz@users.noreply.github.com>
2021-08-20 13:40:17 -0400

    Debug

--- dist/index.js
+++ dist/index.js
@@ -28411,7 +28411,7 @@ class ProjectActions {
     async removeItem(octokit, projectId, itemId) {
         try {
             const mutation = `
-                mutation {
+                mutation removeItem($projectId: String!, $itemId: String!) {
                     deleteProjectNextItem(
                         input: {
                           projectId: $projectId
@@ -28454,7 +28454,7 @@ class ProjectActions {
 
     async findProjectId(octokit, context, projectNumber) {
         try {
-            const query = `query findProjectId($owner: String!, $projectNumber: String!) {
+            const query = `query findProjectId($owner: String!, $projectNumber: Int!) {
                 organization(login: $owner) {
                     projectNext(number: $projectNumber) {
                         id
@@ -28475,7 +28475,7 @@ class ProjectActions {
     async createItem(octokit, projectId, contentId) {
         try {
             const mutation = `
-                mutation {
+                mutation createItem($projectId: String!, $contentId: String!) {
                     addProjectNextItem(input: {projectId: $projectId contentId: $contentId}) {
                         projectNextItem {
                             id
@@ -28491,206 +28491,6 @@ class ProjectActions {
             throw new Error(`Error creating item for item ID [${contentId}] in project ${projectId}: ${error.message}`);
         }
     }
-
-    // async findProjectCardId(octokit, projectNumber, context) {
-    //     const { itemQuery, projectCardsPath, itemType, itemNumber } = context;
-    //     try {
-    //         const query = `{
-    //             ${this.scopedProjectQuery("repo", context)} {
-    //                 ${itemQuery} {
-    //                     projectCards {
-    //                         edges {
-    //                             node {
-    //                                 project {
-    //                                     number
-    //                                 },
-    //                                 id
-    //                             }
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }`;
-
-    //         console.log(`Query for project cards:\n${query}`);
-    //         const response = await octokit(query);
-    //         console.log(`Response from query for project cards:\n${JSON.stringify(response, null, 2)}`);
-    //         const projectCards = _.get(response, projectCardsPath);
-    //         let cardId = null;
-    //         if (projectCards) {
-    //             const matchingCard = _.find(projectCards, function(card) {
-    //                 return (projectNumber == _.get(card, 'node.project.number'));
-    //             });
-
-    //             if (matchingCard) {
-    //                 cardId = _.get(matchingCard, 'node.id');
-    //             }
-    //         }
-    //         return cardId;
-    //     } catch (error) {
-    //         throw new Error(`Error querying project card for ${itemType} number ${itemNumber}: ${error.message}`);
-    //     }
-    // }
-
-    // async findColumnIdForColumnName(octokit, projectScope, projectNumber, columnName, context) {
-    //     try {
-    //         const query = `{
-    //             ${this.scopedProjectQuery(projectScope, context)} {
-    //                 project(number: ${projectNumber}) {
-    //                     columns(first: 50) {
-    //                         nodes {
-    //                             name,
-    //                             id
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }`;
-
-    //         console.log(`Query for project columns:\n ${query}`)
-    //         const response = await octokit(query);
-    //         console.log(`Response for project columns query:\n ${JSON.stringify(response, null, 2)}`);
-    //         const nodePath = this.scopedProjectColumnsPath(projectScope);
-    //         const columns = _.get(response, nodePath);
-    //         console.log(`Columns returned at nodePath '${nodePath}': ${JSON.stringify(columns, null, 2)}`);
-    //         if (columns) {
-    //             console.log(`Searching for column named '${columnName}'`);
-    //             const targetColumn = _.find(columns, function(column) {
-    //                 return (columnName == _.get(column, 'name'));
-    //             });
-    //             if (targetColumn) {
-    //                 console.log(`Found targetColumn: ${JSON.stringify(targetColumn)}`);
-    //                 const targetColumnId =  _.get(targetColumn, 'id');
-    //                 console.log(`Found target column ID: ${targetColumnId}`);
-    //                 return targetColumnId;
-    //             }
-    //         }
-    //         console.log(`Column not found for columName ${columnName}`);
-    //         return null;
-    //     } catch (error) {
-    //         throw new Error(`Error finding column ID for column name ${columnName}: ${error.message}`);
-    //     }
-    // }
-
-    // async createCard(octokit, projectColumnId, contentId) {
-    //     try {
-    //         const mutation = `
-    //             mutation {
-    //                 addProjectCard(input: { projectColumnId: "${projectColumnId}", contentId: "${contentId}" }) {
-    //                     cardEdge {
-    //                         node {
-    //                             id
-    //                         }
-    //                     }
-    //                 }
-    //             }`;
-    //         console.log(`Create card mutation: ${mutation}`);
-    //         const response = await octokit(mutation); // Octokit will throw an error if GraphQL returns any error messages
-    //         console.log(`Create card response:\n${JSON.stringify(response)}`);
-    //     } catch (error) {
-    //         throw new Error(`Error creating card with content ID [${contentId}] in project column [${projectColumnId}]: ${error.message}`);
-    //     }
-    // }
-
-    // // GraphQL query segment to allow searching for projects belonging to an organization, user, or repository.
-    // scopedProjectQuery(projectScope, context) {
-    //     switch(projectScope) {
-    //         case 'org':
-    //             return `organization(login: "${context.owner}")`;
-    //         case 'user':
-    //             return `user(login: "${context.owner}")`;
-    //         case 'repo':
-    //             return `repository(owner: "${context.owner}", name: "${context.repo}")`;
-    //         default:
-    //             throw new Error(`Invalid projectScope ${projectScope}. Expected: org, user, or repo`);
-    //     }
-    // }
-
-    // // For extracting columns data from a project columns GraphQL query
-    // scopedProjectColumnsPath(projectScope) {
-    //     switch(projectScope) {
-    //         case 'org':
-    //             return 'organization.project.columns.nodes';
-    //         case 'user':
-    //             return 'user.project.columns.nodes';
-    //         case 'repo':
-    //             return 'repository.project.columns.nodes';
-    //         default:
-    //             throw new Error(`Invalid projectScope ${projectScope}. Expected: org, user, or repo`);
-    //     }
-    // }
-
-    // // Label removed; Find any associated project card and delete it.
-    // async handleUnlabeled(octokit, projectNumber, labelToMatch, context) {
-    //     if (context.label == labelToMatch) {
-    //         const { itemType, itemNumber } = context;
-
-    //         if (!projectNumber) {
-    //             throw new Error(`projectNumber is required.`);
-    //         }
-
-    //         const projectCards = await this.findProjectCardsForPayloadItem(octokit, context);
-    //         if (!projectCards) {
-    //             console.log(`No project cards found for ${itemType} number ${itemNumber}`);
-    //             return;
-    //         }
-
-    //         const cardToRemove = _.find(projectCards, function(card) {
-    //             return (projectNumber == _.get(card, 'node.project.number'));
-    //         });
-    //         if (!cardToRemove) {
-    //             console.log(`No card found in project ${projectNumber} for the given ${itemType}`);
-    //             return;
-    //         }
-
-    //         const cardId = _.get(cardToRemove, 'node.id');
-    //         console.log(`Removing card [${cardId}] for ${itemType} number ${itemNumber} from project ${projectNumber}`);
-    //         await this.removeCard(octokit, cardId);
-    //     }
-    // }
-
-    // async findProjectCardsForPayloadItem(octokit, context) {
-    //     const { owner, repo, projectCardsPath, itemQuery, itemType, itemNumber } = context;
-    //     try {
-    //         const query = `{
-    //             repository(owner: "${owner}", name: "${repo}") {
-    //                 ${itemQuery} {
-    //                     projectCards {
-    //                         edges {
-    //                             node {
-    //                                 project {
-    //                                     number
-    //                                 },
-    //                                 id
-    //                             }
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }`;
-    //         console.log(`Query for project cards:\n${query}`);
-    //         const response = await octokit(query); // Octokit will throw an error if GraphQL returns any error messages
-    //         console.log(`Response to query for project cards:\n${JSON.stringify(response)}`);
-    //         return _.get(response, projectCardsPath);
-    //     } catch (error) {
-    //         throw new Error(`Error finding project cards for ${itemType} number ${itemNumber}: ${error.message}`);
-    //     }
-    // }
-
-    // async removeCard(octokit, cardId) {
-    //     try {
-    //         const mutation = `mutation {
-    //             deleteProjectCard(input: {cardId: "${cardId}"}) {
-    //                 deletedCardId
-    //             }
-    //         }`;
-    //         console.log(`Remove card mutation:\n${mutation}`);
-    //         const response = await octokit(mutation); // Octokit will throw an error if GraphQL returns any error messages
-    //         console.log(`Remove card response:\n${JSON.stringify(response)}`);
-    //     } catch (error) {
-    //         throw new Error(`Error removing card ${cardId}`);
-    //     }
-    // }
 
     normalizedGithubContext(githubContext) {
         const context = {
